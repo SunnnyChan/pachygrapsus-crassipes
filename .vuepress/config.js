63ac84095aac0b71e7861d071bce3091e4bb8b70
sunnychan <sunnnychan@gmail.com>
2020-06-04 13:54:56 +0800

    sunnychan

---
+++ .vuepress/config.js
@@ -0,0 +1,38 @@
+// .vuepress/config.js
+module.exports = {
+  title: '冒号课堂',  // 设置网站标题
+  description : "",
+  base : '/pachygrapsus-crassipes/',
+  themeConfig: {
+    nav : [
+        { text: 'Github', link: 'http://www.github.com/sunnnychan' }
+    ],
+    sidebar: [
+ 
+      {
+        title: '内容摘录',   // 必要的
+        path: '/content/',      // 可选的
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 1,    // 可选的, 默认值是 1
+      },
+      {
+        title: 'Python演示代码',   // 必要的
+        path: '/python/',      // 可选的
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 1,    // 可选的, 默认值是 1
+      },
+      {
+        title: '封面说明',   // 必要的
+        path: '/cover/',      // 可选的
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 1,    // 可选的, 默认值是 1
+      },
+      {
+        title: '作者简介',   // 必要的
+        path: '/author/',      // 可选的
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 1,    // 可选的, 默认值是 1
+      }
+    ]
+  }
+}
--- README.md
+++ README.md
@@ -5,19 +5,35 @@
 ![](_pic/pachygrapsus-crassipes-cover.png)  
 
 
-## [封面说明 - Pachygrapsus Crassipes ](pachygrapsus-crassipes.md)  
+## [封面说明 - Pachygrapsus Crassipes ](cover/)  
 
-## [作者简介](author.md)
+## [作者简介](author/)
 
 ## [内容](content/)
 
 ### 第1章 探索性数据分析
 ### 第2章 数据和抽样分布
-### 第3章 统计实验与显著性检验
+### 第3章 统计实验与显著性检验  
+
+[3.1 A/B测试](content/03/0301.md)  
+[3.2 假设检验](content/03/0302.md)  
+[3.3 重抽样](content/03/0303.md)    
+[3.4 统计显著性和p值](content/03/0304.md)  
+[3.5 t 检验](content/03/0305.md)  
+[3.6 多重检验](content/03/0306.md)    
+[3.7 自由度](content/03/0307.md)    
+[3.8 方差分析](content/03/0308.md)    
+[3.9 卡方检验](content/03/0309.md)    
+[3.10 多臂老虎机算法](content/03/0310.md)    
+[3.11 检验效能和样本规模](content/03/0311.md)    
+[3.12 小结](content/03/0312.md)   
+
 ### 第4章 回归与预测
 ### 第5章 分类
 ### 第6章 统计机器学习
 ### 第7章 无监督学习  
 
-## Python 代码
+## Python 代码  
+
+
 
